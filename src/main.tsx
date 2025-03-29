47d0213de3dab03e12620d20ac858200c2946244
fmel025 <00018720@uca.edu.sv>
2025-03-28 23:19:22 -0600

    💡 Add comments for main example

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import { View, Text } from "react-native";
-import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useSQLiteContext } from "expo-sqlite";
-import { useDatabase } from "@Hooks/useDatabase";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { useSQLiteContext } from "expo-sqlite";
+import { View, Text } from "react-native";
+
+import { useDatabase } from "@Hooks/useDatabase";
 
 function Main() {
   const db = useSQLiteContext();
   const drizzleDb = useDatabase(db);
 
+  // This is only an example and not final code
   const { data } = useLiveQuery(drizzleDb.query.categories.findMany());
 
   return (
