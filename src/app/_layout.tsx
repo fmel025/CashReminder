4bd6acdeaa18c049bbb99ead94c0808aac7a0f80
fmel025 <00018720@uca.edu.sv>
2025-04-02 22:50:43 -0600

    ✨ Add sqlite configuration for root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,27 @@
+import { DATABASE_NAME } from "@env";
+import { useDatabaseConnection } from "@Hooks/useDatabaseConnection";
 import { Slot } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { Suspense } from "react";
+import { ActivityIndicator } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const { success, error } = useDatabaseConnection();
+
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Slot />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <Suspense fallback={<ActivityIndicator size="large" />}>
+      <SQLiteProvider
+        databaseName={DATABASE_NAME}
+        options={{ enableChangeListener: true }}
+        useSuspense
+      >
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Slot />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </SQLiteProvider>
+    </Suspense>
   );
 }
