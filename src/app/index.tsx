7b33ae72fb3981fa5e81a16b0e2f0dc8117d0044
fmel025 <00018720@uca.edu.sv>
2025-04-02 22:52:58 -0600

    ✨ Add use database implementation on index page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,12 @@
+import { useDatabase } from "@Hooks/useDatabase";
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import { View, Text } from "react-native";
 
 export default function Home() {
+  const { db, drizzleDb } = useDatabase();
+
+  useDrizzleStudio(db);
+
   return (
     <View>
       <Text>Home</Text>
