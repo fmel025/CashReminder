3ec83e083906b473ba2708580a1300c82a54d6d5
fmel025 <00018720@uca.edu.sv>
2025-04-03 22:32:21 -0600

    ✨ Add useEffect to check on database migrations success

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,12 +2,16 @@ import { DATABASE_NAME } from "@env";
 import { useDatabaseConnection } from "@Hooks/useDatabaseConnection";
 import { Slot } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { ActivityIndicator } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   const { success, error } = useDatabaseConnection();
+
+  useEffect(() => {
+    console.log(success, error);
+  }, [success, error]);
 
   return (
     <Suspense fallback={<ActivityIndicator size="large" />}>
