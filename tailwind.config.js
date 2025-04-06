1af9c07f7ef57282f51f5ccdd8e8b8c68a1bc06a
fmel025 <00018720@uca.edu.sv>
2025-04-06 00:33:03 -0600

    🔧 Add jit configuration for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
+  mode: "jit",
   theme: {
     extend: {},
   },
