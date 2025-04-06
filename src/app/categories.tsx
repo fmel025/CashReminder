a1f22c0575c6394976e2792261eca70641d5742c
fmel025 <00018720@uca.edu.sv>
2025-04-06 00:33:24 -0600

    ⚗️ Add categories view test on tailwind

--- src/app/categories.tsx
+++ src/app/categories.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, FlatList, Pressable } from "react-native";
 import React from "react";
 import { useDatabase } from "@Hooks/useDatabase";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
@@ -10,7 +10,22 @@ export default function Categories() {
   return (
     <View className="flex-1 items-center justify-center bg-green-100">
       <Text className="text-2xl font-bold color-blue-900 text-center w-11/12">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+        <FlatList
+          scrollEnabled
+          data={[...data]}
+          renderItem={({ item }) => (
+            <>
+              <Pressable
+                className="mt-2 bg-purple-200 p-4 rounded-md"
+                onPress={() => alert(`Has seleccionado ${item.name}`)}
+              >
+                <Text className="text-xl font-medium color-blue-900 text-center">
+                  {item.name}
+                </Text>
+              </Pressable>
+            </>
+          )}
+        />
       </Text>
     </View>
   );
